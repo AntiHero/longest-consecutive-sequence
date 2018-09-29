0cc23266147db2ed463fe085fb23da3c54f081d1
AntiHero <prophreaker@gmail.com>
2018-09-29 23:02:16 +0300

    longest-consecutive/feature: function implementn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    if (array.length == 0) {
+        return 0;
+    }
+
+    array.sort((a, b) => a - b);
+
+    let count = 1;
+    let tmp = 1;
+    const len = array.length;
+
+    for (let i = 0; i < len; i++) {
+        if (array[i] + 1 == array[i + 1]) {
+            tmp++;
+        } else if (array[i] == array[i + 1]) {
+            continue;
+        } else {
+            if (tmp > 1) {
+                if (count < tmp) {
+                    count = tmp;
+                }
+                tmp = 1;
+            }
+        }
+    }
+    return count;
 }
+
